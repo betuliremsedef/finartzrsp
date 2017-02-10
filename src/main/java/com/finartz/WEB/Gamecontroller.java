7fb58d8fbcd2e7f3a939e2b1bf43b5dd1cb91428
betuliremsedef <b.irem.sedef@gmail.com>
2017-02-10 20:42:05 +0200

    Update Gamecontroller.java

--- src/main/java/com/finartz/WEB/Gamecontroller.java
+++ src/main/java/com/finartz/WEB/Gamecontroller.java
@@ -6,10 +6,13 @@ import com.finartz.logic.GameLogic;
 import org.springframework.beans.BeansException;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.ApplicationContextAware;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+
 
 /**
  * GameController
@@ -17,49 +20,72 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/game")
 public class Gamecontroller implements ApplicationContextAware {
+	
+	private ApplicationContext applicationContext;
+	private String firstPlayerMove=null;
+	private String secondPlayerMove=null;
 
-    private ApplicationContext applicationContext;
+	
+	
+	@RequestMapping(value = "/play/firstPlayerName/{playerName1}/secondPlayerName/{playerName2}", method = {RequestMethod.POST,RequestMethod.GET})
+	public GameResponse play(@PathVariable ("playerName1") String playerName1,@PathVariable ("playerName2") String playerName2,GameRequest request) {
 
-    @RequestMapping(value = "/play", method = RequestMethod.POST)
-    public GameResponse play(@RequestBody GameRequest request) {
+		GameLogic gameLogic = applicationContext.getBean(GameLogic.class);
 
-        GameLogic gameLogic = applicationContext.getBean(GameLogic.class);
+		
+		firstPlayerMove=gameLogic.getPlayerMove();
+		secondPlayerMove=gameLogic.getPlayerMove();
+		
+		request.setFirstPlayerName(playerName1);
+		request.setSecondPlayerName(playerName2);
+		
+		request.setFirstPlayerMove(firstPlayerMove);
+		request.setSecondPlayerMove(secondPlayerMove);
+		
+		
+		boolean same = gameLogic.isSame(request.getFirstPlayerMove(), request.getSecondPlayerMove());
+		if (same) {
+			GameResponse gameResponse = new GameResponse();
+			gameResponse.setFirstPlayerWins(false); gameResponse.setSecondPlayerWins(false); gameResponse.setWinner("");
+			gameResponse.setMessage(request.getFirstPlayerName() + " and " + request.getSecondPlayerName() + " made same move.");
+			return gameResponse;
+		}
 
-        boolean same = gameLogic.isSame(request.getFirstPlayerMove(), request.getSecondPlayerMove());
-        if (same) {
-            GameResponse gameResponse = new GameResponse();
-            gameResponse.setFirstPlayerWins(false); gameResponse.setSecondPlayerWins(false); gameResponse.setWinner("");
-            gameResponse.setMessage(request.getFirstPlayerName() + " and " + request.getSecondPlayerName() + " made same move.");
-            return gameResponse;
-        }
 
+		boolean firstPlayerWin = gameLogic.firstWins(request.getFirstPlayerMove(), request.getSecondPlayerMove());
 
-        boolean firstPlayerWin = gameLogic.firstWins(request.getFirstPlayerMove(), request.getSecondPlayerMove());
+		GameResponse gameResponse = new GameResponse();
+		if (firstPlayerWin) {
+			gameResponse.setFirstPlayerWins(true);
+			gameResponse.setSecondPlayerWins(false);
+		} else {
+			gameResponse.setFirstPlayerWins(false);
+			gameResponse.setSecondPlayerWins(true);
+		}
 
-        GameResponse gameResponse = new GameResponse();
-        if (firstPlayerWin) {
-            gameResponse.setFirstPlayerWins(true);
-            gameResponse.setSecondPlayerWins(false);
-        } else {
-            gameResponse.setFirstPlayerWins(false);
-            gameResponse.setSecondPlayerWins(true);
-        }
+		gameResponse.setWinner(firstPlayerWin ? request.getFirstPlayerName() : request.getSecondPlayerName());
 
-        gameResponse.setWinner(firstPlayerWin ? request.getFirstPlayerName() : request.getSecondPlayerName());
+		prepareWinningMessage(request, firstPlayerWin, gameResponse);
 
-        prepareWinningMessage(request, firstPlayerWin, gameResponse);
+		return gameResponse;
+	}
 
-        return gameResponse;
-    }
+	
+	
+	
+	private void prepareWinningMessage(GameRequest request, boolean firstPlayerWin, GameResponse gameResponse) {
+		String winningMessage = " won the game";
+		gameResponse.setMessage(firstPlayerWin ? request.getFirstPlayerName() + winningMessage: request.getSecondPlayerName() + winningMessage);
+	}
 
-    private void prepareWinningMessage(GameRequest request, boolean firstPlayerWin, GameResponse gameResponse) {
-        String winningMessage = " won the game";
-        gameResponse.setMessage(firstPlayerWin ? request.getFirstPlayerName() + winningMessage: request.getSecondPlayerName() + winningMessage);
-    }
+	
+	
+	
+	
+	
+	@Override
+	public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
 
-    @Override
-    public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
-
-        this.applicationContext = applicationContext;
-    }
+		this.applicationContext = applicationContext;
+	}
 }
