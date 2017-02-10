297e75ebee751813910acd5f7919821e67432e46
betuliremsedef <b.irem.sedef@gmail.com>
2017-02-10 20:47:29 +0200

    Update RockscissorspaperApplicationTests.java

--- src/test/java/com/finartz/RockscissorspaperApplicationTests.java
+++ src/test/java/com/finartz/RockscissorspaperApplicationTests.java
@@ -5,12 +5,36 @@ import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import com.finartz.logic.GameLogic;
+
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class RockscissorspaperApplicationTests {
 
+	private String param1=null;
+	private String param2=null;
 	@Test
 	public void contextLoads() {
+		GameLogic gamelogic=new GameLogic();
+		
+	//Playing game with player parameters:
+		
+		
+		
+	//localhost:8080/rockscissorspaper/game/play?firstPlayerName="{param1}"&secondPlayerName="{param2}"
+		
+		
+		
+	//Getting player move :
+		param1= gamelogic.getPlayerMove();
+		System.out.println("Player move for player1:"+param1);
+		
+		param2=gamelogic.getPlayerMove();
+		System.out.println("Player move for player2:"+param2);
+		
+		
+		
+		
 	}
 
 }
